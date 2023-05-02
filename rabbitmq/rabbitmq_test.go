6cf134527ac81d30a29a2241af57dad221bd9650
ruoye <13138461914@163.com>
2023-05-02 22:39:36 +0800

    初始化连接2

--- rabbitmq/rabbitmq_test.go
+++ rabbitmq/rabbitmq_test.go
@@ -7,8 +7,12 @@ import (
 type RabbitMQ struct {
 	Conn      *amqp.Connection
 	Channel   *amqp.Channel
-	Queuename string
+	QueueName string
 	Exchange  string
 	Key       string
 	Mqurl     string
+}
+
+func NewRabbitMQ(queueName string, exchange string, key string) *RabbitMQ {
+	return &RabbitMQ{QueueName: queueName, Exchange: exchange, Key: key, Mqurl: MQURL}
 }
